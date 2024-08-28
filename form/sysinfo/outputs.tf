01c17b947cacb507271fe48ee9e39c0ba9c060a2
baldpope <jofficer@gmail.com>
2024-08-28 00:29:27 -0500

    testin outputs.tf

--- form/sysinfo/outputs.tf
+++ form/sysinfo/outputs.tf
@@ -1,22 +1,22 @@
 output "srx_id" {
     description = "ID of the SRX appliance"
-    value = junos_system_information.srx.id
+    value = data.junos_system_information.srx.id
 }
 
 output "srx_hardware_model" {
-    value = junos_system_information.srx.hardware_model
+    value = data.junos_system_information.srx.hardware_model
 }
 
 output "srx_os_name" {
-    value = junos_system_information.srx.os_name
+    value = data.junos_system_information.srx.os_name
 }
 
 output "srx_os_version" {
-    value = junos_system_information.srx.os_version
+    value = data.junos_system_information.srx.os_version
 }
 
 output "srx_serial_number" {
-    value = junos_system_information.srx.serial_number
+    value = data.junos_system_information.srx.serial_number
 }
 
 output "hostname" {
