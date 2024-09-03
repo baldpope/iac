ed38b8e30bb3e696f89995ecf70c70168cee6528
baldpope <jofficer@gmail.com>
2024-09-02 22:42:28 -0500

    adding security zone config

---
+++ form/sec_zone/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    junos = {
+      source = "jeremmfr/junos"
+    }
+  }
+}
+
+# Configure the Junos Provider
+provider "junos" {
+  ip         = var.junos_host
+  sshkey_pem = file(var.sshkeyfile)
+  username      = var.junos_user
+}
+
+resource "junos_security_zone" "sec_zone" {
+    name                = var.sec_zone
+    inbound_protocols   = var.ingress_protocol_list
+    inbound_services    = var.ingress_service_list
+
+}
+
+resource "junos_interface_logical" "interface" {
+    name            = var.interface_name
+    description     = var.interface_description
+    security_zone   = junos_security_zone.sec_zone.name
+    family_inet {
+        dhcp { 
+            } 
+    }
+}
+
---
+++ form/sec_zone/variables.tf
@@ -0,0 +1,43 @@
+variable "junos_host" {
+    type = string
+    description = "the management IP address of the device you want to manage"
+}
+
+variable "junos_user" {
+    default = "terraform"
+    description = "user for terraform netconf configurations"  
+}
+
+variable "sshkeyfile" {
+    description = "path to private key"
+}
+
+variable "interface_name" {
+    description = "logical interface id eg: ge-0/0/0.0"
+}
+
+variable "interface_description" {
+    description = "verbal description for interface"
+  
+}
+
+variable "ingress_protocol_list" {
+    type = list
+    description = "list of allowed inbound protocols"
+}
+
+variable "ingress_service_list" {
+    type = list
+    description = "list of allowed inbound services"
+}
+
+variable "family_inet_block" {
+    description = "JSON block for inet definition"
+    type = string
+    default = "dhcp {}"
+}
+
+variable "sec_zone" {
+    description = "name for security zone"
+  
+}
